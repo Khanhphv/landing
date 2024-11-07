71d37d7e14f8d196074b31b4064501ad0e9d1d99
Khanhphv <vietkhanh1310@gmail.com>
2024-11-07 12:13:10 +0700

    remove yarnrc

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,33 +1,3 @@
-import Link from '@/components/Link'
-import Tag from '@/components/Tag'
-import siteMetadata from '@/data/siteMetadata'
-import { formatDate } from 'pliny/utils/formatDate'
-import NewsletterForm from 'pliny/ui/NewsletterForm'
-import Support from '@/components/sections/support'
-import About from '@/components/sections/about'
-import Features from '@/components/sections/feaures'
-import Brand from '@/components/sections/brand'
-import FairPlay from '@/components/sections/fairplay'
-
-const MAX_DISPLAY = 5
-
 export default function Home({ posts }) {
-  return (
-    <>
-      <div id="home">
-        <Brand />
-      </div>
-      <FairPlay />
-      <div id="features">
-        <Features />
-      </div>
-
-      <div id="support">
-        <Support />
-      </div>
-      <div id="about">
-        <About />
-      </div>
-    </>
-  )
+  return <></>
 }
