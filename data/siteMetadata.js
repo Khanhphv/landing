35383d137fce28cb824bb015e48682e3ac4b1bff
Khanhphv <vietkhanh1310@gmail.com>
2024-11-08 16:24:24 +0700

    update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   theme: 'dark', // system, dark or light
   siteRepo: 'https://www.skydevs.wtf',
   siteLogo: `${process.env.BASE_PATH || ''}/logo.png`,
-  socialBanner: ``,
+  socialBanner: `/banner.png`,
   mastodon: '',
   email: 'address@yoursite.com',
   github: 'https://github.com',
